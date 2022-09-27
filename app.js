429a30db028b520717d6428164e387daef9fec41
lkovacevic1 <lukablok4598@gmail.com>
2022-09-27 20:56:59 +0200

    Napravljen servis IdeaService sa motodom find koja vraca sve dosadasnje ideje i metodom create koja krira ideju

---
+++ app.js
@@ -0,0 +1,61 @@
+const feathers = require('@feathersjs/feathers')
+const express = require('@feathersjs/express')
+const socketio = require('@feathersjs/socketio')
+const moment = require('moment')
+
+// Idea Service
+class IdeaService{
+    constructor(){
+        this.ideas = [];
+    }
+
+    async find(){
+        return this.ideas;
+    }
+
+    async create(data){
+        const idea = {
+            id: this.ideas.length,
+            text: data.text,
+            tech: data.tech,
+            viewer: data.viewer
+        };
+
+        idea.time = moment().format('h:mm:ss a')
+
+        this.ideas.push(idea);
+        
+        return idea;
+    }
+}
+
+const app = express(feathers());
+
+// Parse JSON
+app.use(express.json());
+// Configure Socket.io realtime APIs
+app.configure(socketio());
+// Enable REST service
+app.configure(express.rest());
+// Register services
+app.use('/ideas', new IdeaService());
+
+// New connnections connect to stream channel
+app.on('connection', (connection) => {
+    app.channel('stream').join(connection);
+});
+// Publis events to stream
+app.publish(data => app.channel('stream'));
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT).on('listening', () => {
+    console.log(`Realtime server runnign on port ${PORT}`);
+})
+
+// app.service('ideas').create({
+//     text: 'Build a cool app',
+//     texh: 'Node.js',
+//     viewer: 'John Doe',
+//     time: moment().format('h:mm:ss a')
+// });
